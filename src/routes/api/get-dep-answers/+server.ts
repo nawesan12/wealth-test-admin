40a112d46482f8e0f582d24e152ea72087fcef8a
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-08 00:48:03 -0300

    a

---
+++ src/routes/api/get-dep-answers/+server.ts
@@ -0,0 +1,18 @@
+import { json, error } from '@sveltejs/kit';
+import { prisma } from '$lib/database/client';
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+	// TODO: Detect if Angelo is the request owner
+
+	const surveys = await prisma.surveyAnswer.findMany();
+
+	if (!surveys) {
+		return error(400, 'We could not read the surveys from database!');
+	}
+
+	if (surveys.length === 0) {
+		return json({ msg: 'There are not surveys!' });
+	}
+
+	return json(surveys);
+}
