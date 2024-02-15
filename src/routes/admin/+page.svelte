f1477574cc51d53c1fc602000976939465300f7f
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-14 23:05:51 -0300

    asd

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import ServiceCard from '@/components/ServiceCard/service-card.svelte';
 	import ServiceTitle from '@/components/ServiceCard/service-title.svelte';
+	import TokenAssurance from '@/components/TokenAssurance/token-assurance.svelte';
 </script>
 
-// ACA
-<main class="flex flex-col items-center justify-center gap-8 mx-auto w-full h-full">
-	<section class="grid grid-cols-1 gap-4 max-w-xl lg:grid-cols-3 lg:gap-8">
-		<ServiceCard>
-			<ServiceTitle>DEP</ServiceTitle>
-			<a href="/admin/dep">Acceder</a>
-		</ServiceCard>
-	</section>
-</main>
+<TokenAssurance>
+	<main class="flex flex-col items-center justify-center gap-8 mx-auto w-full h-full">
+		<section class="grid grid-cols-1 gap-4 max-w-xl lg:grid-cols-3 lg:gap-8">
+			<ServiceCard>
+				<ServiceTitle>DEP</ServiceTitle>
+				<a href="/admin/dep">Acceder</a>
+			</ServiceCard>
+		</section>
+	</main>
+</TokenAssurance>
