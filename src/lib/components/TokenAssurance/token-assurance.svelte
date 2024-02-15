91a7d1418361c7d389215f1cc8dc414ad539c2cb
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-14 23:02:59 -0300

    asd

---
+++ src/lib/components/TokenAssurance/token-assurance.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		//@ts-ignore
+		const authenticated = JSON.parse(localStorage.getItem('admin-access-token'));
+		if (authenticated !== undefined) {
+			return;
+		}
+
+		goto('/');
+	});
+</script>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -3,6 +3,7 @@
 	import ServiceTitle from '@/components/ServiceCard/service-title.svelte';
 </script>
 
+// ACA
 <main class="flex flex-col items-center justify-center gap-8 mx-auto w-full h-full">
 	<section class="grid grid-cols-1 gap-4 max-w-xl lg:grid-cols-3 lg:gap-8">
 		<ServiceCard>
