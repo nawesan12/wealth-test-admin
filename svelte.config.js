00fdd87c1557bb3872cee4b65ceddd9a7e9ae5f7
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2024-12-10 13:26:55 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      runtime: 'nodejs18.x' // Specify the runtime here
+    }),
 		alias: {
 			'@/*': './src/lib'
 		}
